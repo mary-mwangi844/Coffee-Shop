92d0a7708a9f8ea93886f35386078e481c5c2bcf
mary-mwangi844 <mary-mwangi844@users.noreply.github.com>
2026-07-08 16:05:37 +0300

    Remove conflicting lockfile and fix next.config.js for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: '/home/maryann/Downloads/mayas-coffee-shop'
+  // Remove absolute path for Netlify compatibility
 }
 
 module.exports = nextConfig
